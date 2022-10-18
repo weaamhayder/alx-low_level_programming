42acbb311e735a30146af595b9d95f6ea005cff5
weaamhayder <weaaam_123@hotmail.com>
2022-10-18 14:06:25 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - main func
+ * @n: input num
+ * Return: 1 and prints + if n is greater than zero
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+return (0);
+}
+
