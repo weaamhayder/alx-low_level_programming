d71d024a6192f6d6b8bceb799784aec805627e2c
weaamhayder <weaaam_123@hotmail.com>
2022-10-18 14:10:37 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - main func
+ * @n: input num
+ * Return: abs value
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n * -1);
+}
+else
+{
+return (n);
+}
+}
+
---
+++ 0x02-functions_nested_loops/6-abs.c~
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - main func
+ * @n: input num
+ * Return: abs value
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n * -1);
+}
+else
+{
+return (n);
+}
+}
